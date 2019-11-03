b7751e4085f2fd3874abf98ffa777f36ea3410cb
anhquangtong2000@gmail.com <quanganh*2000>
2019-11-03 18:26:11 +0700

    12

---
+++ day11/1.js
@@ -0,0 +1,15 @@
+// alert("Nhúng js từ file bên ngoài")
+console.log("In ra cửa sổ console")
+console.log(1+1)
+
+var a = 5;
+var b = 7;
+var c = b - a;
+var d = b + a;
+var e = b * a;
+var f = b / a;
+console.log("Hieu cua phep tinh: " + c);
+console.log("Tổng cua phep tinh: " + d);
+console.log("Tích cua phep tinh: " + e);
+console.log("Thương cua phep tinh: " + f);
+
